00af8efeaaef55d9bc44f2af534f63c10351caf1
Sierra Kenchiova <skenchiova@yahoo.com>
2022-09-23 15:40:07 -0700

    Added "Last Updated" with correct Time and Day

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
+function formatDate(timestamp) {
+	let date = new Date(timestamp);
+	let hours = date.getHours();
+	if (hours < 10) {
+		hours = `0${hours}`;
+	}
+	let minutes = date.getMinutes();
+	if (minutes < 10) {
+		minutes = `0${minutes}`;
+	}
+	let days = [
+		"Sunday",
+		"Monday",
+		"Tuesday",
+		"Wednesday",
+		"Thursday",
+		"Friday",
+		"Saturday",
+	];
+	let day = days[date.getDay()];
+	return `${day} ${hours}:${minutes}`;
+}
+
 function displayTemperature(response) {
 	console.log(response.data);
+	let dateTime = document.querySelector("#date-time");
 	let currentTemperature = document.querySelector("#current-temperature");
 	let currentCity = document.querySelector("#current-city");
 	let currentCondition = document.querySelector("#current-condition");
@@ -10,6 +34,7 @@ function displayTemperature(response) {
 	currentCondition.innerHTML = response.data.weather[0].description;
 	humidity.innerHTML = response.data.main.humidity;
 	wind.innerHTML = Math.round(response.data.wind.speed);
+	dateTime.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 let weatherKey = "23e5c32242e8082ee2e13b831cfd6fb5";
