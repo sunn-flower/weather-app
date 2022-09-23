f5e052c3795c890b483f9e927c006f0a133e1fef
Sierra Kenchiova <skenchiova@yahoo.com>
2022-09-23 15:16:36 -0700

    Connected Real Time Data to Elements on Page

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 function displayTemperature(response) {
-	console.log(response.data.main.temp);
+	console.log(response.data);
 	let currentTemperature = document.querySelector("#current-temperature");
+	let currentCity = document.querySelector("#current-city");
+	let currentCondition = document.querySelector("#current-condition");
+	let humidity = document.querySelector("#humidity");
+	let wind = document.querySelector("#wind");
 	currentTemperature.innerHTML = Math.round(response.data.main.temp);
+	currentCity.innerHTML = response.data.name;
+	currentCondition.innerHTML = response.data.weather[0].description;
+	humidity.innerHTML = response.data.main.humidity;
+	wind.innerHTML = Math.round(response.data.wind.speed);
 }
 
 let weatherKey = "23e5c32242e8082ee2e13b831cfd6fb5";
