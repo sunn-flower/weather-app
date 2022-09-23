cecef3ac0bc40f9e8ffc8b9575b4585df0afa928
Sierra Kenchiova <skenchiova@yahoo.com>
2022-09-23 14:49:59 -0700

    Connected API For Current Weather

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+function displayTemperature(response) {
+	console.log(response.data.main.temp);
+	let currentTemperature = document.querySelector("#current-temperature");
+	currentTemperature.innerHTML = Math.round(response.data.main.temp);
+}
 
+let weatherKey = "23e5c32242e8082ee2e13b831cfd6fb5";
+let cityName = "New York";
+let tempUnit = "imperial";
+let weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${weatherKey}&units=${tempUnit}`;
+
+axios.get(weatherUrl).then(displayTemperature);
